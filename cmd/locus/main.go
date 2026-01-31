7a5669bdd341b68f256b35ac4f4cf676c88e6dc2
Versifine <versifine@163.com>
2026-01-31 11:59:38 +0800

    feat: initialize project structure with Go module and directory layout

--- cmd/locus/main.go
+++ cmd/locus/main.go
@@ -1,13 +1,7 @@
-// Package main 是 Locus 代理服务器的入口点
-// 负责读取配置、启动监听
 package main
 
-import (
-	"log"
-)
+import "fmt"
 
 func main() {
-	log.Println("Locus 代理服务器启动中...")
-	// TODO: 读取配置
-	// TODO: 启动 TCP 监听
+	fmt.Println("Hello, Locus!")
 }
